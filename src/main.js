a5841efd57cc68aef5509e3eadc80d287afe7f1e
UkYiJS <ukyi.js@gmail.com>
2020-12-20 17:45:30 +0900

    feat: main 추가

--- src/main.js
+++ src/main.js
@@ -1,24 +1,9 @@
 import { DefaultApolloClient } from '@vue/apollo-composable';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import ApolloClient from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/css/normalize.css';
+import { apolloClient } from './config';
 import router from './router';
 import store from './store';
 
-const httpLink = createHttpLink({
-  uri: 'https://gzhm34nn6g.execute-api.ap-northeast-2.amazonaws.com/api/graphql',
-});
-const cache = new InMemoryCache();
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-});
-
-createApp(App)
-  .provide(DefaultApolloClient, apolloClient)
-  .use(store)
-  .use(router)
-  .mount('#app');
+createApp(App).provide(DefaultApolloClient, apolloClient).use(store).use(router).mount('#app');
